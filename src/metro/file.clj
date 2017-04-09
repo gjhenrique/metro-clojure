55f4fa5dda4942a5d366e55cde5563a09e8c457c
Guilherme Henrique <gjhenrique@gmail.com>
2017-04-09 13:19:39 -0300

    Simplify output of file

--- src/metro/file.clj
+++ src/metro/file.clj
@@ -1,45 +1,33 @@
 (ns metro.file
-  (:require [sandbox.core :refer :all]
-            [clojure.java.io :as io]
+  (:require [clojure.java.io :as io]
             [clojure.string :as str])
   (:gen-class))
+
+(def regex-line-separator #"\*")
 
 (defn file-contents
   [file-name]
   (-> (io/resource (str file-name ".txt"))
       (io/file)
-      (slurp)))
-
-
-(defn find-connections
-  [line-name station lines]
-  (map :name (filter #(and (some #{station} (:stations %))
-                           (not (= line-name (:name %))))
-                      lines)))
-
-(defn association-between-lines
-  [line lines]
-  (assoc line :stations
-         (map #(assoc {}
-                      :name %
-                      :connections (find-connections (:name line) % lines))
-              (:stations line))))
-
+      (slurp)
+      (str/split-lines)))
 
 (defn loop-lines
   [file-name]
-  (partition 2 (partition-by
-                #(re-find #"\d+ - \w*" %)
-                (str/split-lines (file-contents file-name)))))
+  (partition 2
+             (partition-by
+                #(re-find (re-pattrn (str regex-line-separator "\w*")) %)
+                (file-contents file-name))))
 
-(defn build-stations
-  [file-name]
-  (for [[station-info stations] (loop-lines file-name)]
-    (let [info (map str/trim (str/split (first station-info) #"-"))]
-      (assoc {} :position (read-string (first info)) :name (second info) :stations stations))))
+(defn filter-stations
+  [line-info]
+  (let [name (ffirst line-info)
+        stations (second line-info)]
+    (assoc {} :name (str/replace name regex-line-separator "") :stations stations)))
 
 ;; TODO: put functions under the methods
 (defn format-stations
   [file-name]
-   (let [stations (build-stations file-name)]
-     (map #(association-between-lines % stations) stations)))
+  (map
+   filter-stations
+   (loop-lines file-name)))
