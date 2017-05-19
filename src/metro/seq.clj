ceb16dff22447ded270d1acdb0540f1f7a66daf8
Guilherme Henrique <gjhenrique@gmail.com>
2017-05-19 01:39:47 -0300

    Finally I got it.
    It's 1:40AM and I'm literally crying
    
    OMG. I <3 programming

--- src/metro/seq.clj
+++ src/metro/seq.clj
@@ -4,22 +4,39 @@
 (deftype MetroGraph [state]
   clojure.lang.ISeq
   (more [self]
-    (let [new-state (metro.algorithm/traverse-subway-graph state)]
-      (if (:end new-state)
-        '()
-        (MetroGraph. new-state))))
+    (let [new-state (next self)]
+    (if new-state
+      new-state
+      '())))
 
   (next [self]
-    (:current-node (rest state)))
+    ;; (:current-node (rest state)))
+    ;; (when-let [state (.state (rest self))]
+    ;;   (list (:current-node state))))
+      (let [new-state (metro.algorithm/traverse-subway-graph state)]
+        (when-not (:end new-state)
+          (MetroGraph. new-state))))
+;; (when-let [new-state (.state (rest self))] ;;   (MetroGraph. new-state)))
 
+  ;; Only this is realized
   (first [self] (:current-node state))
 
-  (seq [self] (when (seq? self) self))) 
+  (seq [self] (when self self))) 
+
 
 (defmethod print-method MetroGraph [o, ^java.io.Writer w]
-  (.write w (str "MetroGraph")))
+  (.write w (str (.state o)))) 
 
 (def graph (metro.graph/build-subway-graph
              [{:name "Blue" :stations ["A" "B" "C" "D"]}])) 
 
-;; (seq-graph [{:name "Blue" :stations ["A" "B" "C" "D"]}])
+(filter #(= % "A") (MetroGraph. (metro.algorithm/traverse-subway-graph graph))) 
+
+(class 
+(-> (MetroGraph. (metro.algorithm/traverse-subway-graph graph))
+    rest rest) 
+) 
+
+(map identity (MetroGraph. (metro.algorithm/traverse-subway-graph graph))) 
+(count (MetroGraph. (metro.algorithm/traverse-subway-graph graph))) 
+
