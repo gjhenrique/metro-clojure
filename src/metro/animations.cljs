c5a1429796d6be613014cf7039ec20a4253c5712
Guilherme Henrique <gjhenrique@gmail.com>
2017-08-06 11:34:36 -0300

    Add animations to paint the nodes

--- src/metro/animations.cljs
+++ src/metro/animations.cljs
@@ -1,27 +1,55 @@
 (ns metro.animations
-  (:require [metro.algorithm :as alg]
+  (:require [metro.algorithm]
             [metro.graph]
+            [metro.seq]
             [clojure.string]
             [cljsjs.cytoscape]))
 
 (enable-console-print!)
 
-(def g (metro.graph/build-subway-graph
-        [{:name "Blue" :stations ["A" "B" "E"]}
-         {:name "Red" :stations ["C" "B" "G"]}]))
+(def config [{:name "Blue" :stations ["A" "B" "G"]}
+             {:name "Green" :stations ["D" "B" "F" "H"]}
+             {:name "Red" :stations ["C" "B" "E" "H"]}])
 
-(defn graph-attrs [graph]
-  {:nodes (map (fn [n] {:data {:id n} :classes (clojure.string/join " " (alg/lines g n))}) (loom.graph/nodes graph))
+(def g (metro.graph/build-subway-graph config))
+
+(defn graph-attrs
+  [graph]
+  {:nodes (map
+           (fn [n] {:data {:id n}
+                    :classes (clojure.string/join " " (metro.graph/lines graph n))})
+           (metro.graph/stations graph))
    :edges (map (fn [e] {:data {:id (clojure.string/join e) :source (first e) :target (second e)}})
-        (loom.graph/edges graph))})
+               (loom.graph/edges graph))})
 
-(js/cytoscape (clj->js {:container (.getElementById js/document "graph")
-                        :elements (graph-attrs g)
-                        :boxSelectionEnabled true
-                        :layout {:name "breadthfirst" :directed true}
-                        :style [{:selector "node" :style {"label" "data(id)"}}
-                                {:selector "edge" :style {"curve-style" "bezier" "target-arrow-shape" "triangle"
-                                                          "width" 4 "line-color" "#888" "target-arrow-color" "#333"}}
-                                {:selector ".Blue" :style {"background-color" "blue"}}
-                                {:selector ".Red" :style {"background-color" "red"}}
-                                {:selector ".Red.Blue" :style {"background-color" "#101"}}]}))
+(def cy (js/cytoscape (clj->js {:container (.getElementById js/document "graph")
+                                :elements (graph-attrs g)
+                                :boxSelectionEnabled false
+                                :layout {:name "breadthfirst" :directed true}
+                                :style [{:selector "node" :style {"label" "data(id)" "text-margin-x" "3px"
+                                                                  "padding" "10px" "text-halign" "right" "text-valign" "center"}}
+                                        {:selector "edge" :style {"curve-style" "bezier" "target-arrow-shape" "triangle"
+                                                                  "width" 4 "line-color" "#888" "target-arrow-color" "#333"}}
+                                        {:selector ".Blue" :style {"background-color" "blue"}}
+                                        {:selector ".Red" :style {"background-color" "red"}}
+                                        {:selector ".Green" :style {"background-color" "green"}}
+                                        {:selector ".Red.Blue" :style {"background-color" "#101"}}
+                                        {:selector ".Red.Blue.Green" :style {"background-color" "orange"}}
+                                        {:selector ".highlighted" :style {"background-color" "olive" "line-color" "olive"
+                                                                          "target-arrow-color" "olive"
+                                                                          "transition-property" "background-color, line-color, target-arrow-color"
+                                                                          "transition-duration" "0.5s"}}]})))
+
+(defn search-algorithm
+  [cy seqa config]
+  (if (empty? seqa)
+
+    (do
+      (.removeClass (.nodes cy) "highlighted")
+      (js/setTimeout #(search-algorithm cy (metro.seq/seq-graph config) config) 1000))
+
+    (do
+      (.addClass (.getElementById cy (:station (first seqa))) "highlighted")
+      (js/setTimeout #(search-algorithm cy (rest seqa) config) 1000))))
+
+(search-algorithm cy (metro.seq/seq-graph config) config)
