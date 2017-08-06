23ebea0504ab6f5c64b17abf921b74f5e1ff2ae6
Guilherme Henrique <gjhenrique@gmail.com>
2017-08-06 20:21:39 -0300

    Refactor and export function start-animation to be used in js

--- src/metro/animations.cljs
+++ src/metro/animations.cljs
@@ -6,12 +6,29 @@
 
 (enable-console-print!)
 
-(def config [{:name "Blue" :stations ["Sé" "Liberdade" "São Joaquim"]}
-             {:name "Green" :stations ["Sé" "A" "F" "H"]}
-             {:name "Red" :stations ["C" "B" "E" "H"]}])
+(def graph-layout {:name "breadthfirst" :directed true})
 
-(def g (metro.graph/build-subway-graph config))
+(def node-style {:selector "node"
+                 :style {"label" "data(id)" "text-margin-x" "7px"
+                         "padding" "10px" "text-halign" "right" "text-valign" "center"}})
 
+(def edge-style {:selector "edge"
+                 :style {"curve-style" "bezier" "target-arrow-shape" "triangle"
+                         "width" 4 "line-color" "#888" "target-arrow-color" "#333"}})
+
+(def colors-style [{:selector ".Blue" :style {"background-color" "blue"}}
+                   {:selector ".Red" :style {"background-color" "red"}}
+                   {:selector ".Green" :style {"background-color" "green"}}
+                   {:selector ".Red.Blue" :style {"background-color" "#101"}}
+                   {:selector ".Red.Green" :style {"background-color" "#101"}}
+                   {:selector ".Red.Blue.Green" :style {"background-color" "orange"}}])
+
+(def highlighted-style {:selector ".highlighted"
+                        :style {"border-color" "#87CEFA" "border-width" "10"
+                                "line-color" "#87CEFA"
+                                "target-arrow-color" "#87CEFA"
+                                "transition-property" "background-color, line-color, target-arrow-color"
+                                "transition-duration" "0.5s"}})
 (defn graph-attrs
   [graph]
   {:nodes (map
@@ -21,25 +38,12 @@
    :edges (map (fn [e] {:data {:id (clojure.string/join e) :source (first e) :target (second e)}})
                (metro.graph/connections graph))})
 
-(def cy (js/cytoscape (clj->js {:container (.getElementById js/document "graph")
-                                :elements (graph-attrs g)
-                                :boxSelectionEnabled false
-                                :layout {:name "breadthfirst" :directed true}
-                                :style [{:selector "node" :style {"label" "data(id)" "text-margin-x" "3px"
-                                                                  "padding" "10px" "text-halign" "right" "text-valign" "center"}}
-                                        {:selector "edge" :style {"curve-style" "bezier" "target-arrow-shape" "triangle"
-                                                                  "width" 4 "line-color" "#888" "target-arrow-color" "#333"}}
-                                        {:selector ".Blue" :style {"background-color" "blue"}}
-                                        {:selector ".Red" :style {"background-color" "red"}}
-                                        {:selector ".Green" :style {"background-color" "green"}}
-                                        {:selector ".Red.Blue" :style {"background-color" "#101"}}
-                                        {:selector ".Red.Green" :style {"background-color" "#101"}}
-                                        {:selector ".Red.Blue.Green" :style {"background-color" "orange"}}
-                                        {:selector ".highlighted" :style {"border-color" "#87CEFA" "border-width" "10"
-                                                                          "line-color" "#87CEFA"
-                                                                          "target-arrow-color" "#87CEFA"
-                                                                          "transition-property" "background-color, line-color, target-arrow-color"
-                                                                          "transition-duration" "0.5s"}}]})))
+(defn create-cy
+  [container graph]
+  (js/cytoscape (clj->js {:container (.getElementById js/document container)
+                          :elements (graph-attrs graph)
+                          :layout graph-layout
+                          :style (concat [node-style edge-style highlighted-style] colors-style) })))
 
 (defn find-element
   [cy id]
@@ -53,6 +57,7 @@
 
 (defn cy-nodes-and-edges
   [cy stations-seq graph]
+  ;; Removing nodes that don't have successors
   (remove nil?
           ;; flattening only one level
           (apply concat
@@ -61,18 +66,25 @@
                          [(find-element cy (:station station))]])
                       stations-seq))))
 
-(defn search-algorithm
+(defn iterate-animation
   [cy original-seq algorithm-seq]
   (if (empty? algorithm-seq)
 
     (do
-      (.removeClass (.nodes cy) "highlighted")
-      (.removeClass (.edges cy) "highlighted")
-      (js/setTimeout #(search-algorithm cy original-seq original-seq) 500))
+      (.batch cy (fn []
+                   (.removeClass (.nodes cy) "highlighted")
+                   (.removeClass (.edges cy) "highlighted")))
+
+      (js/setTimeout #(iterate-animation cy original-seq original-seq) 500))
 
     (do
       (run! #(.addClass % "highlighted") (first algorithm-seq))
-      (js/setTimeout #(search-algorithm cy original-seq (rest algorithm-seq)) 500))))
+      (js/setTimeout #(iterate-animation cy original-seq (rest algorithm-seq)) 500))))
 
-(let [nodes-edges (cy-nodes-and-edges cy (metro.seq/seq-graph g) g)]
-  (search-algorithm cy nodes-edges nodes-edges))
+(defn ^:export start-animation
+  [container config]
+  (let [graph (metro.graph/build-subway-graph (js->clj config :keywordize-keys true))
+        metro-seq (metro.seq/seq-graph graph)
+        cy (create-cy container graph)
+        nodes-edges (cy-nodes-and-edges cy metro-seq graph)]
+    (iterate-animation cy nodes-edges nodes-edges)))
